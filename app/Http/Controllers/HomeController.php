2218b47bb0ff0c08a20956f8f276f942e4562343
ival-fas <ival@fas.id>
2023-07-03 17:12:30 +0700

    fix: change status

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Actions\Statistics\StoreLinkStatistic;
 use App\Models\Link;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -15,12 +17,13 @@ class HomeController extends Controller
 
     public function link(Request $request, StoreLinkStatistic $statistic, Link $link, string $secret = null)
     {
+        Log::info('link accessed -> '.json_encode($link));
         if (!$link->is_public && $link->secret !== $secret) {
             $statistic->failed()->save($request, $link);
             abort(404);
         }
         $statistic->save($request, $link);
 
-        return redirect()->away($link->full_link, 301);
+        return redirect()->away($link->full_link, Response::HTTP_TEMPORARY_REDIRECT);
     }
 }
